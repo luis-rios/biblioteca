ede223083eb8a2b8f9b5bd8a795cd62b6b052a02
luis-rios <vvrhz11@gmail.com>
2021-02-04 00:33:39 -0600

    creando_sarama_salomon

---
+++ src/components/Catalogo.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'React';
+export default class Catalogo extends Component{
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
