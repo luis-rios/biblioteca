56ac4460eaee936b5e2ff33c9eed5cb2fea2e2b0
luis-rios <vvrhz11@gmail.com>
2021-02-06 19:02:36 -0600

    navbar_terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import '../styles/navbar.css'
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Navbar(props){
-    return (
-        <div>
-            
-        </div>
+    const {
+        image
+    }=props
+    return (        
+        <nav className="body-nav">                
+            <img src={image}/><p>Biblioteca Digital!</p> 
+            <ul className="nav-container" >
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Autores</a></li>
+                <li><a href="#">Series</a></li>
+                <li><a href="#">Paginas</a></li>                    
+            </ul>
+            <form>
+                <FontAwesomeIcon icon={faSearch} style={{paddingRight:'10px', fontSize:'30px'}}/>
+                {/*<input className="input-search" type="search" placeholder="Buscar"/>
+                <button className="button-search">Buscar</button>*/}
+            </form>            
+        </nav>
     )
 }
