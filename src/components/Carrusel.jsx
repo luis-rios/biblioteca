b4c62bbc83034abeded58946f1ad3e85c54720e0
salomon1102 <acaru.211@gmail.com>
2021-03-03 19:16:47 -0600

    Carousel terminado, falta arreglar logo

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -3,45 +3,52 @@ import '../styles/Item.css'
 import Img1 from '../images/elprincipito.jpg'
 import Img2 from '../images/king.jpg'
 import Img3 from '../images/potter.jpg'
+import '../styles/Carousel.css';
 export default function Carrusel(){   
     return(
-        
-            <Carousel>
-            <Carousel.Item>
+        <div className="carousel">
+
+<Carousel className="container">
+            <Carousel.Item >
                 <img
-                    className="image"
+                    className="image1"
                     src={Img1}
-                    alt="First slide"
+                    alt="Primera slide"              
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <Carousel.Caption >
+                    <h3>Detalles</h3>
+                    <p><span>El principito</span> es una narración corta del escritor francés Antoine de Saint-Exupéry, que trata de la historia de un pequeño príncipe que parte de su asteroide a una travesía por el universo, en la cual descubre la extraña forma en que los adultos ven la vida y comprende el valor del amor y la amistad.</p>
                 </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                 <img
-                    className="image"
+                    className="image2"
                     src={Img2}
-                    alt="Second slide"
+                    alt="Segundo slide"
                 />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3><span>Detalles</span></h3>
+                    <p><span>Jack Torrance</span> se convierte en cuidador de invierno en el Hotel Overlook, en Colorado, con la esperanza de vencer su bloqueo con la escritura. Se instala allí junto con su esposa, Wendy, y su hijo, Danny, que está plagado de premoniciones psíquicas. Mientras la escritura de Jack no fluye y las visiones de Danny se vuelven más preocupantes, Jack descubre oscuros secretos del hotel y comienza a convertirse en un maníaco homicida, empeñado en aterrorizar a su familia.</p>
                 </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                 <img
-                    className="image"
+                    className="image3"
                     src={Img3}
-                    alt="Third slide"
+                    alt="Tercero slide"
                 />
 
                 <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                <h3><span>Detalles</span></h3>
+                <p><span>Hogwarts</span>  se prepara para el Torneo de los Tres Magos, en el que competirán tres escuelas de hechicería. Para sorpresa de todos, Harry Potter es elegido para participar en la competencia, en la que deberá luchar contra dragones, internarse en el agua y enfrentarse a sus mayores miedos.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             </Carousel>
         
     )
+
+
+        </div>
+    )
 }
+            
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../styles/navbar.css'
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import image from '../images/digitalb.png';
+import image from '../images/logo.png';
+
 export default function Navbar(props){
     
     return (        
         <nav className="body-nav">                
-            <img src={image} style={{width:'250px'}}/><p>Biblioteca Digital!</p> 
+            <img src={image} className="logo"/>
             <ul className="nav-container" >
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Autores</a></li>
